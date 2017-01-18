78a4535edcbfa12f7f09c92708bb2149614deeac
Luke M Griffith <lukemgriffith@gmail.com>
2017-01-18 23:53:46 +0000

    implemented tick completon postback on JS / API

--- src/TickList/wwwroot/js/main.js
+++ src/TickList/wwwroot/js/main.js
@@ -1,29 +1,142 @@
+var TickList, Inputs
 
+function newTickItemDom(id,text,completed) {
+/*
+    18/01/2017
+    Created a new tick item and appends to the DOM.
 
-
-function newTickItem(id,text) {
-
-    var msg, content
+*/
+    var msg, content, article, inputDom
 
     article = document.createElement("article")
 
     article.className = "tickItem"
-    article.id = id
-    msg =  '    <p>' + text + '</p>'
-    msg += '    <input type="checkbox" id="item' + id + '" class="box"/>'
-    msg += '    <lable for="item' + id + '" class="tab"/>'
-
+    article.id = "tick"+id
+    msg =  '<p>' + text + '</p>'
+    msg += '<input type="checkbox" onclick="handleClick(this)" id="item' + id + '" class="box"/>'
+    msg += '<label for="item' + id + '" class="tab"/>'
 
     article.innerHTML = msg
 
-    console.log(article)
+    inputDom = article.childNodes[1]
 
-    
-    
+    inputDom.checked = completed
+
     content = document.getElementById("content")
 
     content.appendChild(article)
 
 }
 
-newTickItem(4,'This is a new item')
+/*
+newTickItemDom(1,'This is a new item')
+newTickItemDom(2,'This is a new item')
+*/
+
+function getIDFromClass(className) {
+    return className.replace("item","")
+}
+
+
+function handleClick(inputItem){
+
+    var tickID, domObject, tickItem
+
+    tickID = getIDFromClass(inputItem.id)
+
+    TickList.map(function(x){
+        if (x.tickItemID == tickID) {
+            x.completed = !x.completed
+
+            tickItem = x
+        }
+    })
+
+    pushTickListToApi(tickItem)
+
+}
+
+
+function getTickItemFromDom(id) {
+/*
+    18/01/2017
+    Obtains a TickItem via ID, or the TickArray from the DOM.
+*/
+    var domObject 
+
+    if (id) {
+        domObject = document.getElementById("tick"+id)
+    }
+    else {
+        domObject = document.getElementsByClassName("tickItem")
+    }
+
+    return domObject
+
+}
+
+function deleteTickItemFromDom(id){
+/*
+    18/01/2017
+    deletes a tick item from the DOM via the ID
+*/
+    var domObject
+
+    domObject = getTickItemFromDom(id)
+    domObject.remove()
+}
+
+function getTickListFromApi(){
+
+    var req
+
+    function responseListener () { 
+
+        var resp
+
+        resp = JSON.parse(this.responseText)
+
+        resp.map(function(x){
+            TickList.push(x)
+            newTickItemDom(x.tickItemID, x.title, x.completed)
+        })
+    }
+
+    req = new XMLHttpRequest
+
+    req.addEventListener("load", responseListener);
+
+    req.open("GET","api/Tick")
+    req.send()
+
+}
+
+function pushTickListToApi(TickItem) {
+
+    var req
+
+
+    function logResult(){
+        console.log("post completed")
+        console.log(this)
+    }
+
+    req = new XMLHttpRequest
+
+    req.addEventListener("load", logResult);
+
+    req.open("PUT","api/Tick/"+TickItem.tickItemID)
+
+    req.setRequestHeader("Content-type", "application/json")
+
+    jsonBlob = JSON.stringify(TickItem)
+
+    req.send(jsonBlob)
+}
+
+document.getElementsByTagName("input")
+
+TickList = new Array
+
+getTickListFromApi()
+
